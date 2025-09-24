6738d39c336f72d758d480aa6e810762ae378eed
bruce1694 <purushoth3505@gmail.com>
2025-09-24 19:15:19 +0530

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,56 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        # create the genesis block
+        self.new_block(previous_hash="1", proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """Create a new Block and add it to the chain"""
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1]),
+        }
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """Add a new transaction to the list"""
+        self.transactions.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount,
+        })
+        return self.last_block["index"] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """Hash a block"""
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """Simple PoW algorithm"""
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """Check if hash(last_proof, proof) contains 4 leading zeroes"""
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
---
+++ node_server.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from blockchain import Blockchain
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(sender="0", recipient="miner_address", amount=1)
+    block = blockchain.new_block(proof)
+
+    response = {
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
+    }
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required = ["sender", "recipient", "amount"]
+    if not all(k in values for k in required):
+        return "Missing values", 400
+
+    index = blockchain.new_transaction(values["sender"], values["recipient"], values["amount"])
+    response = {"message": f"Transaction will be added to Block {index}"}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {"chain": blockchain.chain, "length": len(blockchain.chain)}
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
